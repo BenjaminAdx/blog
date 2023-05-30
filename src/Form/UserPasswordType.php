90573249a4b319375325d6f4bc6f1042ededcae6
benjaminadx <andrieux.benjamin1@gmail.com>
2023-05-30 09:10:24 +0200

    modif password

--- src/Form/UserPasswordType.php
+++ src/Form/UserPasswordType.php
@@ -6,49 +6,15 @@ use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
-use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 
-class RegistrationType extends AbstractType
+class UserPasswordType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('fullName', TextType::class, [
-                'attr' => [
-                    'class' => 'form-control',
-                    'minlength' => '2',
-                    'maxlength' => '50'
-                ],
-                'label' => 'Nom / Prénom',
-                'label_attr' => [
-                    'class' => 'form-label mb-4'
-                ]
-            ])
-            ->add('pseudo', TextType::class, [
-                'attr' => [
-                    'class' => 'form-control',
-                    'minlength' => '2',
-                    'maxlength' => '50'
-                ],
-                'label' => 'Pseudonyme (facultatif)',
-                'label_attr' => [
-                    'class' => 'form-label mb-4'
-                ],
-                'required' => false
-            ])
-            ->add('email', EmailType::class, [
-                'attr' => [
-                    'class' => 'form-control',
-                ],
-                'label' => 'Adresse email',
-                'label_attr' => [
-                    'class' => 'form-label mb-4'
-                ]
-            ])
             ->add('plainPassword', RepeatedType::class, [
                 'type' => PasswordType::class,
                 'first_options' => [
@@ -64,11 +30,26 @@ class RegistrationType extends AbstractType
                 ],
                 'invalid_message' => 'Les mots de passe ne correspondent pas.'
             ])
+            ->add('newPassword', RepeatedType::class, [
+                'type' => PasswordType::class,
+                'first_options' => [
+                    'label' => 'Nouveau mot de passe',
+                    'attr' => ['class' => 'form-control mt-4']
+                ],
+                'second_options' => [
+                    'label' => 'Confirmer le nouveau mot de passe',
+                    'attr' => ['class' => 'form-control mt-4'],
+                    'label_attr' => [
+                        'class' => 'form-label mt-4'
+                    ]
+                ],
+                'invalid_message' => 'Les mots de passe ne correspondent pas.'
+            ])
             ->add('submit', SubmitType::class, [
                 'attr' => [
                     'class' => 'btn btn-primary mt-4'
                 ],
-                'label' => "S'inscrire"
+                'label' => "Modifier mon mot de passe"
             ]);
     }
 
--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -7,12 +7,9 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
-use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
-use Symfony\Component\Form\Extension\Core\Type\PasswordType;
-use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 
-class RegistrationType extends AbstractType
+class UserType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
@@ -40,35 +37,11 @@ class RegistrationType extends AbstractType
                 ],
                 'required' => false
             ])
-            ->add('email', EmailType::class, [
-                'attr' => [
-                    'class' => 'form-control',
-                ],
-                'label' => 'Adresse email',
-                'label_attr' => [
-                    'class' => 'form-label mb-4'
-                ]
-            ])
-            ->add('plainPassword', RepeatedType::class, [
-                'type' => PasswordType::class,
-                'first_options' => [
-                    'label' => 'Mot de passe',
-                    'attr' => ['class' => 'form-control mt-4']
-                ],
-                'second_options' => [
-                    'label' => 'Confirmer le mot de passe',
-                    'attr' => ['class' => 'form-control mt-4'],
-                    'label_attr' => [
-                        'class' => 'form-label mt-4'
-                    ]
-                ],
-                'invalid_message' => 'Les mots de passe ne correspondent pas.'
-            ])
             ->add('submit', SubmitType::class, [
                 'attr' => [
                     'class' => 'btn btn-primary mt-4'
                 ],
-                'label' => "S'inscrire"
+                'label' => "Modifier mon compte"
             ]);
     }
 
