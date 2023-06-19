84e1fdf2ad112714b59441a979802f69888065ea
benjaminadx <andrieux.benjamin1@gmail.com>
2023-06-19 13:32:41 +0200

    site prod ok, mise en ligne

--- src/Controller/ResetPasswordController.php
+++ src/Controller/ResetPasswordController.php
@@ -164,7 +164,6 @@ class ResetPasswordController extends AbstractController
             ]);
 
         $mailer->send($email);
-
         // Store the token object in session for retrieval in check-email route.
         $this->setTokenObjectInSession($resetToken);
 
